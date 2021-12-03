45583af9f7f121493b4408fab183321d39b72c27
Peter <schmidt.multimedia@gmail.com>
2021-12-03 11:58:55 +0100

    getAge() / 1. Iteratrion

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat("Grizabella", "white", 29); // Klasse --> Objekt / Instanz
+        Cat cat1 = new Cat("Grizabella", "white", 29, true); // Klasse --> Objekt / Instanz
         
         output("Name: " + cat1.getStringAttributes("#name"));
         output("Fellfarbe: " + cat1.getStringAttributes("#color"));
@@ -13,11 +13,11 @@ public class App {
 
         output("------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "black", 35); // Klasse --> Objekt / Instanz
+        Cat cat2 = new Cat("Alonzo", "black", 35, false); // Klasse --> Objekt / Instanz
    
         output("Name: " + cat2.getStringAttributes("#name"));
         output("Fellfarbe: " + cat2.getStringAttributes("#color"));
-        
+
         output("Alter: " + cat2.getAge());
 
     }
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -5,11 +5,13 @@ public class Cat {
     private String name;
     private String furColor;
     private int age;
+    private boolean isFemale;
 
-    public Cat(String name, String furColor, int age) {
+    public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     public String getStringAttributes(String flag){
@@ -23,8 +25,12 @@ public class Cat {
         }  
     }
 
-    public int getAge() {
-        return age;
+    public String getAge() {
+        if (isFemale) {
+            return "This is an inappropriate question!";
+        } else {
+            return Integer.toString(age); 
+        }   
     }
 
   
